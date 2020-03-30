ca9433cd8edf8be880170e2c732c6379c7604d6f
Luiz Amaro <Luizbarueri@gmail.com>
2020-03-30 19:37:41 -0300

    Be_The_Hero App

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Routers from './routers';
+import './global.css';
+
+// JSX (JavaScript XML) isso quando tem html dentro de um script.
+
+function App() {
+   return (<Routers />);
+}
+
+export default App;
